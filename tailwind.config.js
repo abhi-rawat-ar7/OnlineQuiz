ee01bb736cd63e0ac9c33cf98efaaf2fd0dc4446
Abhi Rawat <abhi.rawat.ar7@gmail.com>
2025-07-02 22:49:15 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'], // Add Inter font family
+      },
+    },
+  },
+  plugins: [],
+}
